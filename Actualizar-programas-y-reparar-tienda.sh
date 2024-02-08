9523fc22711ff4dee079727c12f81bbb0112a032
Agustin Bustos Piasentini <63634574+abustosp@users.noreply.github.com>
2024-02-07 22:28:56 -0300

    Add Sudo to remove old snaps

--- Actualizar-programas-y-reparar-tienda.sh
+++ Actualizar-programas-y-reparar-tienda.sh
@@ -30,7 +30,7 @@ set -eu
 
 LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
-        snap remove "$snapname" --revision="$revision"
+        sudo snap remove "$snapname" --revision="$revision"
     done
 
 echo "-------------------------------"
